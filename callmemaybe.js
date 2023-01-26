00cdc1a2f0356541edd5728041a0ce6e72108f09
Slava Ganzin <slava.ganzin@gmail.com>
2023-01-26 12:31:48 +0100

    improve config management

--- callmemaybe.js
+++ callmemaybe.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node-dev
 
-let config = {}
+global.config = {}
 for (const f in require('ramda'))
   global[f] = require('ramda')[f]
 const {readFile, writeFile} = require('fs').promises
-const yaml = require('yaml')
 const dns2 = require('dns2');
-const {homedir} = require('os')
 const {promisify} = require('util')
 const exec = promisify(require('child_process').exec)
 const http = require('http')
-const daemonizeProcess = require('daemonize-process');
+const daemonizeProcess = require('daemonize-process')
+const yaml = require('yaml')
+const { program } = require('commander')
 
 const onetimeServer = ({message, title}) => {
   const server = http.createServer(function (req, res) {
@@ -23,8 +23,6 @@ const onetimeServer = ({message, title}) => {
     }, 100)
   }).listen({port: 80, host: '0.0.0.0'}, () => console.log('one time server up'))
 }
-
-const { program } = require('commander');
 
 program
   .option('--test')
@@ -45,45 +43,7 @@ const pe = x =>
 
 dns2.pp = x => console.log(join('',values(mapObjIndexed((v,k) => `${k} -> ${join(' ', pluck('address',v))}`, groupBy(x => x.name, x.answers)))))
 
-const CONFIG_FILES = [`${homedir()}/.config/callmemaybe.yaml`]
-
-const reloadConfig = async () =>
-  config = yaml.parse(await readFile(CONFIG_FILES[0], 'utf8'))
-
-Promise.all(CONFIG_FILES.map(x => readFile(x).catch(() =>
-  writeFile(x, `settings:
-  udp:
-    port: 53
-    address: 0.0.0.0
-    type: "udp4"  #(Must be either "udp4" or "udp6")
-  tcp:
-    port: 53
-    address: 0.0.0.0
-    type: "udp4"  #(Must be either "udp4" or "udp6")
-
-# Array of dns resolvers. defaults to system resolvers
-  resolvers: ~
-
-#loopback
-localhost:
-  ip: 127.0.0.1
-
-# Params and default values
-# hostname:                     #hostname of your action
-#   ip:          127.0.0.1      #what ip hostname resolve to.
-#   healthcheck: ~              #any command that checks that project is up, so there is no need to run start command
-#   start:       ~              #command that starts your project
-#   folder:      ~              #folder where command will be running
-
-#test endpoints. feel free to remove them
-
-test.callmemaybe:
-  start: callmemaybe --test
-`)
-)))
-.then(reloadConfig)
-.then(() => setInterval(reloadConfig, 1000))
-.then(() => {
+require('./config').then(() => {
 
 let running = []
 
@@ -131,7 +91,8 @@ const server = dns2.createServer({
 
 
     const resolve = dns2.TCPClient({
-      dns: config.settings.resolvers[0] || dns2.getServers()[0]
+      // dns2.getServers()[0]
+      dns: pathOr('1.1.1.1', ['settings', 'resolvers', 0], config)
     })
     const { name } = question;
     const lookup = await resolve(question.name)
